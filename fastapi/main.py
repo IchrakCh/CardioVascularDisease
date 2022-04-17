2d965e0c8e668eeab7623520b680dfbf5ec6d493
Ichrak CHEKKAR <chekkarich@cy-tech.fr>
2022-04-17 18:47:18 +0200

    some changes

--- fastapi/main.py
+++ fastapi/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from matplotlib.cbook import ls_mapper 
 from pydantic import BaseModel
 from typing import Optional
+import numpy as np
 import csv
 import pickle
 from sklearn import metrics
@@ -19,9 +20,10 @@ from model import train, predict
 class Features(BaseModel):
     # Objective Features 
     age : int 
+    gender : int
     height : int
     weight : float
-    gender : int
+    
     # Examination Features 
     #   For Cholestrol & glucose levels :
     #       1: normal / 2:above normal / 3: well above normal
@@ -36,25 +38,20 @@ class Features(BaseModel):
     # Target Variable
     #cardio : bool
     
-# Fonctions utiles pour traitement des données 
-def setId(filename,input):
-    with open(filename, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=';')
+# Fonctions utiles pour rajouter les données au csv 
+def setId(filename):
+    with open(filename, 'r', newline= '') as f:
+        reader = csv.reader(f, delimiter=';')
         all_lines = list(reader)
         newId = int(all_lines[-1][0])+1
-        csvfile.close()
+        f.close()
     return newId
 
 def addToCsv(filename, input):
-    inputRow = input.insert(0,setId(filename, input))
     with open(filename, 'a', newline='') as f:
-        reader = csv.reader(csvfile, delimiter=';')
-        all_lines = list(reader)
-        newId = int(all_lines[-1][0])+1
-        writer_object = csv.writer(f)
-        writer_object.writerow(inputRow)
+        writer_object = csv.writer(f, delimiter=';')
+        writer_object.writerow(input)
         f.close()
-
 app = FastAPI()
 
 @app.get('/')
@@ -68,11 +65,23 @@ async def home():
 async def addExamination(features: Features):
     
     result = {**features.dict()}
-    print(type(result))
-    input = list(result.values())
-    print(type(input), len(input))
     # add informations into the dataset !
-    #addToCsv("../data/cardio_train.csv",input)
+
+    input = []
+    input.append(setId("../data/cardio_train.csv"))
+    
+    listResult = list(result.values())
+
+    for value in listResult :
+        input.append(value)
+
+    Xnew = []
+    Xnew.append(input)
+    XnewArray = np.array(Xnew)
+    resultPrediction = predict(XnewArray)
+    input.append(resultPrediction[0])
+    print(input)
+    addToCsv("../data/cardio_train.csv",input)
 
 # Retraining the ML model using the new lines added to it 
 @app.post('/api/model/retrain')
@@ -89,9 +98,19 @@ async def retrain():
 
 async def getprediction(features: Features):
     result = {**features.dict()}
-    input = list(result.values())
-    print(len(input))
-    return input
+    input = []
+    input.append(setId("../data/cardio_train.csv"))
+    listResult = list(result.values())
+    for value in listResult :
+        input.append(value)
+    Xnew = []
+    Xnew.append(input)
+    XnewArray = np.array(Xnew)
+    resultPrediction = predict(XnewArray)
+    input.append(resultPrediction[0])
+    print(input)
+    addToCsv("../data/cardio_train.csv",input)
+    return resultPrediction[1]
 
 # Get metrics from the model 
 @app.get('/api/model/metrics')
--- model/model.py
+++ model/model.py
@@ -5,23 +5,16 @@ from matplotlib.colors import ListedColormap
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import make_moons, make_circles, make_classification
-from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.metrics import classification_report, accuracy_score
-from sklearn import preprocessing
-from sklearn import metrics
 import pickle
-import random
-import csv
 
-filename = "../data/cardio_train.csv"
+
 
 names = [
     "Nearest Neighbors",
@@ -89,49 +82,10 @@ def train(filename):
 
     return nameModel, accuracy, metrics 
 
-trainingResult = train("../data/cardio_train.csv")
-
 def predict(input):
     loaded_model = pickle.load(open("../model/saved_model.pickle", 'rb'))
     result = loaded_model.predict(input)
     if (result == 1):
-        return "You have a risk of a cardio vascular disease"
+        return 1,"You have a risk of a cardio vascular disease"
     else:
-        return "You're okay for now but stay safe"
-
-
-result = {
-  "age": 33,
-  "height": 167,
-  "weight": 67,
-  "gender": 2,
-  "ap_hi": 44,
-  "ap_lo": 55,
-  "cholestrol": 56,
-  "gluc": 45,
-  "smoke": True,
-  "alco": True,
-  "active": True
-}
-
-
-
-def setId(filename):
-    with open(filename, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=';')
-        all_lines = list(reader)
-        newId = int(all_lines[-1][0])+1
-        csvfile.close()
-    return newId
-
-input = []
-input.insert(0,setId("../data/cardio_train.csv"))
-listResult = list(result.values())
-for value in listResult :
-  input.append(value)
-print(input)
-
-Xnew = []
-Xnew.append(input)
-XnewArray = np.array(Xnew)
-print(predict(XnewArray))
+        return 0, "You're okay for now but stay safe"
